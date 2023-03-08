3b649a9e4bdeaeb8bc8fc814123b9afaa8c3f91e
Misha <100841904+Mid1i@users.noreply.github.com>
2023-03-08 16:07:03 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        speed: 700,
+        slidesToShow: 1,
+        easing: 'linear',
+        swipe: false,
+        infinite: true
+    });
+});
